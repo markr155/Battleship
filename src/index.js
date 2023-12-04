dbac31b842e734667e21c0f4f5ff901d45414e0d
markr155 <markricho155@gmail.com>
2023-12-04 20:04:34 +1100

    Made set up board droppable

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const comBoard = gameBoard();
 const game = gameController(playerBoard, comBoard);
 
 const playerSetUpBoard = document.querySelector('.game-setup-board');
-const carrier = document.getElementById('carrier');
+const setUpShips = document.querySelectorAll('.setup-ship-square-container')
 
 const player1Board = document.querySelector('.player1-board');
 const player2Board = document.querySelector('.player2-board');
@@ -19,6 +19,18 @@ render.renderBoard(playerSetUpBoard, playerBoard);
 render.renderBoard(player1Board, playerBoard);
 render.renderBoard(player2Board, comBoard);
 
-carrier.addEventListener('dragstart', (e) => {
-  e.dataTransfer.setDragImage(e.target, -5, 5)
+playerSetUpBoard.addEventListener('drop', (e) => {
+  e.preventDefault();
+  // console.log(e.dataTransfer.mozSourceNode)
+  if (!e.dataTransfer.mozSourceNode.firstChild) console.log(e.dataTransfer.mozSourceNode)
+})
+playerSetUpBoard.addEventListener('dragover', (e) => {
+  e.preventDefault();
+})
+
+// Changes drag image to align with cursor 
+setUpShips.forEach((shipSquare) => {
+  shipSquare.addEventListener('dragstart', (e) => {
+    e.dataTransfer.setDragImage(e.target, 0, 0)
+  })
 })
